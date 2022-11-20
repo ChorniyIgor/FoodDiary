b52343db4a8fe964341d478b9fd06b1761c20034
ChorniyIgor <32174871+ChorniyIgor@users.noreply.github.com>
2022-11-20 18:24:48 +0200

    Updates

--- src/store/Auth/appLogout.js
+++
@@ -1,8 +0,0 @@
-import { logout } from "./AuthSlice";
-
-export const appLogout = () => (dispatch) => {
-  localStorage.removeItem("logoutDate");
-  localStorage.removeItem("localId");
-
-  dispatch(logout());
-};
--- src/store/Auth/autoLogout.js
+++
@@ -1,7 +0,0 @@
-import { appLogout } from "./appLogout";
-
-export const autoLogout = (time) => (dispatch) => {
-  setTimeout(() => {
-    dispatch(appLogout());
-  }, time * 10000);
-};
